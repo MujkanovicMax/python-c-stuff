d42211e7fef8f787e6b3b89bde1b7ff413034b1a
Mujkanovic.Max <mujkanovic.max@physik.uni-muenchen.de>
2019-06-26 12:39:40 +0200

    finished add1.c

--- add1.c
+++ add1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int digits(int num){
 
@@ -8,7 +9,7 @@ int digits(int num){
 		num = num/10;
 		a++;
 	}
-	return a-1;
+	return a;
 
 }
 
@@ -21,19 +22,83 @@ int conc(int x, int y){
 	return x*pow +y;
 
 }
-int power(num,p){
 
-
+unsigned int power10(int p){
+    int pow = 1;
+    
+    if(p == 0){
+        
+        return pow;
+        
+    }
+    
+    
+    for(int i = 0;i<p;i++){
+        
+     pow *=10;   
+    }
+    return pow;
 }
+
+
+void reverse(int *arr,int len){
+ 
+    int tmp = 0;
+    for(int i = 0;i<len/2;i++){
+        
+        tmp = arr[i];
+        arr[i] = arr[len-1-i];
+        arr[len-1-i] = tmp;
+        
+        
+    }
+    
+    
+}
+
 void add1(int *num){
-
-	int a = *num;
-	int d = digits(a);
-	int arr[d-1];
-	for(int i=0;i<d;i++){
-
-		arr[i] = 
+    
+	unsigned int a = *num;
+        
+	unsigned int d = digits(a);
+	unsigned int arr[d];
+        unsigned int fin = 0;
+	for(int i=d-1;i>=0;i--){
+                
+                unsigned int pow = power10(i);
+		arr[i] = a/pow;
+                arr[i]++;
+                a = a%pow;
 	}
 	
+	reverse(arr,d);
+	
+	fin = conc(arr[0],arr[1]);
+	for(int i = 2; i<d;i++){
+         
+                fin = conc(fin,arr[i]);
+            
+        }
+	
+        *num = fin;
+}
 
+
+
+int main(){
+ 
+    
+    
+    unsigned int num = 32340333789;
+    printf("%d\n",num);
+    
+    add1(&num);
+    printf("%d\n",num);
+    
+    
+    
+    
+    
+    return 0;
+    
 }
